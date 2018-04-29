338cb56aa50a5fe290c0aa8f684668514e320a10
Pipe <pipefutbol8@gmail.com>
2018-04-29 11:48:40 -0500

    .

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.get('/', (req, res) => {
   res.send('Invalid Endpoint');
 });
 
-app.set('port', (process.env.PORT || 3006));
+app.set('port', (process.env.PORT || 3000));
 
 app.listen(app.get('port'), function(){
 	console.log('Server started on port '+app.get('port'));
