455dd03045c8727198de842f22261d34c7e9b1cd
Pipe <pipefutbol8@gmail.com>
2018-04-30 00:54:24 -0500

    .

--- config/database.js
+++ config/database.js
@@ -1,7 +1,6 @@
 module.exports = {
-  database: 'mongodb://pipe:hola@ds149279.mlab.com:49279/vigtec-pi' ||            
+  database: 'mongodb://pipe:hola@ds149279.mlab.com:49279/vigtec-pi' ||
+            'mongodb://localhost:27017/vigtec' ||            
             'mongodb://pipe:hola@ds263619.mlab.com:63619/vigtec-pi2',
   secret: 'secret'
 }
-
-//'mongodb://localhost:27017/vigtec'
