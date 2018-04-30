3ee28815ce42c96e0abb32888fd3221395c76c86
Pipe <pipefutbol8@gmail.com>
2018-04-30 00:10:34 -0500

    mejoras

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 module.exports = {
-  database: 'mongodb://pipe:hola@ds149279.mlab.com:49279/vigtec-pi' || 
-            'mongodb://localhost:27017/vigtec' ||
+  database: 'mongodb://pipe:hola@ds149279.mlab.com:49279/vigtec-pi' ||            
             'mongodb://pipe:hola@ds263619.mlab.com:63619/vigtec-pi2',
   secret: 'secret'
 }
+
+//'mongodb://localhost:27017/vigtec'
